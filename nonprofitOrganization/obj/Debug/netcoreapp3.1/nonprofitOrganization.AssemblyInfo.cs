ace602fd846733853d4d08ab2ca82f5d03ee6967
DerrickPhillips <derrickrphillips@yahoo.com>
2021-04-08 20:42:54 -0400

    Switched to net 3.1
    Switched project to net 3.1

--- nonprofitOrganization/obj/Debug/netcoreapp3.1/nonprofitOrganization.AssemblyInfo.cs
+++ nonprofitOrganization/obj/Debug/netcoreapp3.1/nonprofitOrganization.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("c2f43389-2c99-4a9a-b829-a101028bbb43")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("nonprofitOrganization")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]

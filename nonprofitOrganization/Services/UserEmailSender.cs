c6a7f3b880e2628b5ed784c72cd849de293f90d5
DerrickPhillips <derrickrphillips@yahoo.com>
2021-04-18 17:18:47 -0400

    Added Email Conformation
    Used SendGrid to setup Email conformation.

---
+++ nonprofitOrganization/Services/UserEmailSender.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace nonprofitOrganization.Services
+{
+    public class UserEmailSender : IEmailSender
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public UserEmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor, UserManager<IdentityUser> userManager)
+        {
+            Options = optionsAccessor.Value;
+            _userManager = userManager;
+        }
+
+        public AuthMessageSenderOptions Options { get; } 
+        public ClaimsPrincipal User { get; private set; }
+
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            return Execute(Options.SendGridKey, subject, message, email);
+        }
+
+        public Task Execute(string apiKey, string subject, string message, string email)
+        {
+            ClaimsPrincipal currentUser = this.User;
+            var currentUserEmail = currentUser.FindFirst(ClaimTypes.Email).Value;
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(currentUserEmail, Options.SendGridUser),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(email));
+            msg.SetClickTracking(false, false);
+
+            return client.SendEmailAsync(msg);
+        }
+    }
+}

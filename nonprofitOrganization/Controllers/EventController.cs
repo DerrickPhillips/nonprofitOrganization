1667999bd0a17e68889c45e2d476b0861cdd6ac6
DerrickPhillips <derrickrphillips@yahoo.com>
2021-04-20 21:32:24 -0400

    Deployment Problem
    Hopefully This Fixed the deployment problem

--- nonprofitOrganization/Controllers/EventController.cs
+++ nonprofitOrganization/Controllers/EventController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using nonprofitOrganization.Data;
 
 namespace nonprofitOrganization.Controllers
 {
